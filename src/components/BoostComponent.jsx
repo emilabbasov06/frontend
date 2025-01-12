36cdd8cccc18371b208154a963d2eaa2d47c3bb4
emilabbasov06 <emilabb13@gmail.com>
2025-01-12 18:17:50 +0400

    setting up vercel

--- src/components/BoostComponent.jsx
+++ src/components/BoostComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BoostComponent = () => {
   return (
-    <div className="mp-center mp-box-shadow">
+    <div className="mp-center flex justify-center items-center mp-box-shadow">
       <h1>Helloooooo Booost</h1>
     </div>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className="mp-center mp-box-shadow">
+    <div className="mp-center flex justify-center items-center mp-box-shadow">
       <h1>Home</h1>
     </div>
   )
