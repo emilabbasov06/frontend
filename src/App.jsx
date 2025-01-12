829a484d9b20d540d9689456e8b2fb4124aadfa5
emilabbasov06 <emilabb13@gmail.com>
2025-01-12 17:02:27 +0400

    added home screen

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
           <div className='absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]'></div>
         </div>
 
-        <div className='container mx-auto p-2'>
+        <div className='container mx-auto'>
           <Home />
         </div>
       </div>
